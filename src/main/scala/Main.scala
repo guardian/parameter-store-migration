16be4512e26243f8205c60d1c2ebf8c9703a1290
tomrf1 <tom.forbes@theguardian.com>
2019-02-11 21:48:26 +0000

    send to parameter store

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,48 +1,55 @@
 import java.io.File
 
 import com.typesafe.config._
-import services.{CredentialsProvider, S3}
+import services.{CredentialsProvider, S3, Ssm}
 
 object Main extends App {
   def info = Console.err.println(
     """
-      |run s3 <profile> <bucket> <path>
+      |====
+      |Migrates a Typesafe (HOCON) config file to AWS Parameter Store.
       |
-      |run local <path>
+      |To migrate a file from S3:
+      |  sbt "run s3 <profile> <bucket> <path>"
+      |
+      |Or to migrate a local file:
+      |  sbt "run local <profile> <path>"
     """.stripMargin
   )
 
-  if (args.isEmpty) info
+  args.toList match {
+    case "s3" :: profile :: bucket :: path :: Nil => put(fromS3(profile, bucket, path), profile)
 
-  val mode = args(0)
+    case "local" :: profile :: path :: Nil => put(fromLocal(path), profile)
 
-  mode match {
-    case "s3" =>
-      args.toList match {
-        case _ :: profile :: bucket :: path :: Nil => s3(profile, bucket, path)
-        case _ => info
-      }
-    case "local" =>
-      args.toList match {
-        case path :: Nil => local(path)
-      }
     case _ => info
   }
 
-  def s3(profile: String, bucket: String, path: String) = {
-    val raw = S3.getObject(bucket, path, CredentialsProvider(profile))
+  def fromS3(profile: String, bucket: String, path: String): Map[String,String] = {
+    val credentialsProvider = CredentialsProvider(profile)
+
+    val raw = S3.getObject(bucket, path, credentialsProvider)
     val config = ConfigFactory.parseString(raw)
 
-    val parameters = Parameters.fromConfig(config)
-
-    println(parameters)
+    Parameters.fromConfig(config)
   }
 
-  def local(path: String) = {
+  def fromLocal(path: String) = {
     val config = ConfigFactory.parseFile(new File(path))
 
-    val parameters = Parameters.fromConfig(config)
-
-    println(parameters)
+    Parameters.fromConfig(config)
   }
+
+  def put(parameters: Map[String,String], profile: String) = {
+    println(s"Migrating:\n${pretty(parameters)}")
+
+    val ssm = new Ssm(CredentialsProvider(profile))
+    parameters foreach (ssm.put _).tupled
+  }
+
+  def pretty(parameters: Map[String,String]): String =
+    parameters
+      .toSeq
+      .sortBy(_._1)
+      .foldLeft("") { case (acc, (k,v)) => s"$acc\n$k -> $v" }
 }
